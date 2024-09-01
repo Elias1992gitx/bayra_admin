a76748dbb89c5961f67e860fd70bde6b71d3ea05
New User <newuser@example.com>
2024-09-01 22:28:38 +0300

    song part commit

--- src/pages/AddSong.jsx
+++ src/pages/AddSong.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useState } from 'react'
 
 const AddSong = () => {
-  return (
-    <form className="flex flex-col items-start gap-8 text-gray-600">
-      <div className="flex gap-8">
+  const [image, setImage] = useState(false)
+  const [song, setSong] = useState(false)
+  const [name, setName] = useState('')
+  const [desc, setDesc] = useState('')
+  const [album, setAlbum] = useState('none')
+  const [loading, setLoading] = useState(false)
+  const [albumData, setAlbumData] = useState([])
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
+
+  return loading ? (
+    
+    <div className='grid place-items-center min-h-[80vh]'>
+      <div className='w-16 h-16 place-self-center border-4 border-grid-400 border-t-green-800 rounded-full animate-spin '></div>
+    </div>
+
+  ) : (
+
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-start gap-8 text-gray-600"
+    >
+      <div className="gap-2 overflow-hidden md:flex">
         <div className="flex flex-col gap-4">
           <p>Upload Song</p>
-          <input type="file" id="song" accept="audio/* hidden" />
+          <input
+            onChange={(e) => setSong(e.target.files[0])}
+            type="file"
+            id="song"
+            accept="audio/* hidden"
+          />
           <label htmlFor="song">
             <img
-              src={assets.upload_song}
+              src={song ? assets.upload_added : assets.upload_song}
               className="w-28 cursor-pointer"
               alt=""
             />
@@ -19,10 +47,15 @@ const AddSong = () => {
 
         <div className="flex flex-col gap-4">
           <p>Upload Image</p>
-          <input type="file" id="image" accept="image/* hidden" />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            accept="image/* hidden"
+          />
           <label htmlFor="image">
             <img
-              src={assets.upload_area}
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
               className="w-28 cursor-pointer"
               alt=""
             />
@@ -30,23 +63,41 @@ const AddSong = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-2.5 w-80">
+      <div className="flex flex-col gap-2.5 w-30 sm:w-80">
         <p>Song Name</p>
         <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
           className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]"
           placeholder="Type song name"
           type="text"
         />
       </div>
 
-      <div className="flex flex-col gap-2.5 w-80">
+      <div className="flex flex-col gap-2.5 w-30 sm:w-80">
         <p>Song Description</p>
         <input
+          onChange={(e) => setDesc(e.target.value)}
+          value={desc}
           className="bg-transparent outline-green-600 border-2 border-gray-400 p-2"
           placeholder="Type song description"
           type="text"
         />
       </div>
+
+      <div className="flex flex-col gap-2.5">
+        <p>Album</p>
+        <select onChange={(e) => setAlbum(e.target.value)} defaultValue= {album} className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px] p-2">
+          <option value="none">None</option>
+        </select>
+      </div>
+
+      <button
+        type="submit"
+        className="text-base bg-green-950 text-white py-2.5 px-14 cursor-pointer rounded-lg"
+      >
+        ADD
+      </button>
     </form>
   )
 }
